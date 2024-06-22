a4d6c68c8797564aea2b07aa35ac1c44d4faed7f
Aleksandr <sanya.romaniuta@gmail.com>
2024-06-22 15:17:30 +0300

    Добавлена кнокпка поиска словарей

--- components/dictionary/HomePanel.tsx
+++ components/dictionary/HomePanel.tsx
@@ -1,13 +1,15 @@
 "use client";
 
 import { createDictionary } from "@/api/serverActions/dictionaries";
-import { AddIcon } from "@chakra-ui/icons";
+import { AddIcon, Search2Icon } from "@chakra-ui/icons";
 import { Button, HStack, Input, useToast } from "@chakra-ui/react";
+import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 
 export default function HomePanel() {
     const [query, setQuery] = useState("");
     const toast = useToast();
+    const router = useRouter();
 
     async function handleCreate() {
         await createDictionary({ name: query });
@@ -19,16 +21,28 @@ export default function HomePanel() {
         });
     }
 
+    function handleSearch() {
+        if (!query) {
+            router.push(`/dictionaries`);
+        }
+
+        router.push(`/dictionaries?q=${query}`);
+    }
+
     return (
         <HStack>
             <Input
-                placeholder={"Введите имя нового словаря"}
+                placeholder={"Имя словаря"}
                 onChange={e => setQuery(e.target.value)}
                 value={query}
                 w={300}
             ></Input>
             <Button onClick={handleCreate}>
                 <AddIcon />
+            </Button>
+
+            <Button onClick={handleSearch}>
+                <Search2Icon />
             </Button>
         </HStack>
     );
